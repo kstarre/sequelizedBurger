2abbb2728b65f2c38b9a818970c22c1a633950d1
Katie Starrett <starrett.k.e@gmail.com>
2017-08-15 02:29:14 -0400

    add final jawsdb info

--- models/burger.js
+++ models/burger.js
@@ -8,21 +8,3 @@ module.exports = function(sequelize, DataTypes) {
 	});
 	return Burger;
 };
-
-/*let burger = {
-	selectAll: (cb) => {
-		orm.selectAll("burgers", res => {
-			cb(res);
-		});
-	},
-	insertOne: (col, val, cb) => {
-		orm.insertOne("burgers", col, val, res => {
-			cb(res);
-		});
-	},
-	updateOne: (newColVal, condition, cb) => {
-		orm.updateOne("burgers", newColVal, condition, res => {
-			cb(res);
-		});
-	}
-};*/
